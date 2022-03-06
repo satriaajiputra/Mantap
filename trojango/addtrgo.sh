f67a799c91926faa662b0fbf623c030231d67682
Satria Aji Putra <satria.10118068@mahasiswa.unikom.ac.id>
2022-03-07 03:20:44 +0700

    Update addtrgo.sh

--- trojango/addtrgo.sh
+++ trojango/addtrgo.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl ipinfo.io/ip | grep $MYIP )
+#IZIN=$( curl ipinfo.io/ip | grep $MYIP )
 if [ $MYIP = $MYIP ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
@@ -31,23 +31,24 @@ else
 domain=$IP
 fi
 trgo="$(cat ~/log-install.txt | grep -w "TrojanGo" | cut -d: -f2|sed 's/ //g')"
-until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
-		read -rp "Password : " -e user
-		user_EXISTS=$(grep -w $user /etc/trojan-go/akun.conf | wc -l)
+until [[ $user =~ ^[a-zA-Z0-9_-]+$ && ${user_EXISTS} == '0' ]]; do
+                read -rp "Password : " -e user
+                user_EXISTS=$(grep -w $user /etc/trojan-go/akun.conf | wc -l)
 
-		if [[ ${user_EXISTS} == '1' ]]; then
-			echo ""
-			echo -e "Username ${RED}${user}${NC} Already On VPS Please Choose Another"
-			exit 1
-		fi
-	done
+                if [[ ${user_EXISTS} == '1' ]]; then
+                        echo ""
+                        echo -e "Username ${RED}${user}${NC} Already On VPS Please Choose Another"
+                        exit 1
+                fi
+        done
 read -p "Expired (Days) : " masaaktif
+read -p "SNI : " sni
 sed -i '/"'""$uuid""'"$/a\,"'""$user""'"' /etc/trojan-go/config.json
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 echo -e "### $user $exp" >> /etc/trojan-go/akun.conf
 systemctl restart trojan-go.service
-link="trojan-go://${user}@${domain}:${trgo}/?sni=${domain}&type=ws&host=${domain}&path=/trojango&encryption=none#$user"
+link="trojan-go://${user}@${domain}:${trgo}/?sni=${sni}&type=ws&host=${domain}&path=/trojango&encryption=none#$user"
 clear
 echo -e ""
 echo -e "=======-TROJAN-GO-======="
